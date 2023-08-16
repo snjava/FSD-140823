d7f06253286f258cc94a9eb07b2b79589778c0ba
snjava <scnaikwadi2@gmail.com>
2023-08-16 08:20:59 +0530

    Basic Rules and Concepts
    Basic Rules and Concepts

---
+++ code/corejava/basic/First.java
@@ -0,0 +1,17 @@
+public class First {
+	public static void main(String ar[]) {
+		System.out.println("hello, From First class");
+	}
+}
+// public, class, static, void -> Keywords
+// First, main, String, System, ar, out, println -> Identifiers
+
+class Second {
+	public static void main(String ar[]) {
+		System.out.println("hello, From Second class");
+	}
+}
+
+class Three {
+
+}
