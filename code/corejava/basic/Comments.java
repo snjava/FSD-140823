f82ccfd0bbaf29b1741afee2ed70eae6d42abc73
snjava <scnaikwadi2@gmail.com>
2023-08-17 09:44:31 +0530

    Data Type intro
    Data Type intro

---
+++ code/corejava/basic/Comments.java
@@ -0,0 +1,14 @@
+/**
+This class is use to represent the comments in java.
+ 	
+*/
+public class Comments {
+	public static void main(String args[])
+	{
+		System.out.println("Hi");
+		System.out.println("Hello");
+		/* System.out.println("Good Morning");
+		 System.out.println("Bye");
+		*/
+	}
+}
---
+++ code/corejava/basic/DataType.java
@@ -0,0 +1,9 @@
+public class DataType {
+	public static void main(String args[]) {
+		byte id = 10;
+		double salary = 45455.54;
+
+		System.out.println(id);
+		System.out.println(salary);	
+	}
+}
