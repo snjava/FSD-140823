1acf65c995cf2489ea331f7c16753b9f37856d8c
snjava <scnaikwadi2@gmail.com>
2023-08-28 09:55:03 +0530

    1-D Array
    1-D Array

---
+++ code/corejava/basic/Array1D_1.java
@@ -0,0 +1,25 @@
+public class Array1D_1 {
+	public static void main(String args[]) {
+		double percent[];
+		percent = new double[5];
+		percent[0] = 78.76;
+		percent[1] = 67.16;
+		percent[2] = 87.96;
+		percent[3] = 81.61;
+		percent[4] = 77.72;
+
+		System.out.println(percent.length); // array size
+		// last index = array.length - 1
+		System.out.println(percent.length - 1); // array last index		
+
+		for(int index = 0 ; index<=4 ; index++) {
+			System.out.println(percent[index]);
+		}
+		System.out.println("=========");
+		for(double val : percent) {
+			System.out.println(val);
+		}
+	}
+}
+
+
