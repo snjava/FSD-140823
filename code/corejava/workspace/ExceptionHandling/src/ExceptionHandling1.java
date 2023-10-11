042d55133c68e966bcfcaff1d020ec6c0e778529
snjava <scnaikwadi2@gmail.com>
2023-10-11 08:28:08 +0530

    Try and Catch
    Try and Catch

---
+++ code/corejava/workspace/ExceptionHandling/src/ExceptionHandling1.java
@@ -0,0 +1,28 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class ExceptionHandling1 {
+	public static void main(String[] args) {
+		int array[] = {14,4,0,54,34,8,32};
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter Index : ");
+		// If you are providing a multiple catch block for a single try then,
+		// you cannot handle the parent exception before child
+		try {	
+			int index = scan.nextInt();
+			int value = array[index];
+			int square = value * value;
+			System.out.println("Square : " + square);
+		}
+		catch(ArrayIndexOutOfBoundsException e) {
+			System.out.println("Invalid Index.. Please try again...");
+		}
+		catch(InputMismatchException e) {
+			System.out.println("Invalid Input.. Please try with valid value....");
+		}
+		catch(Exception e) {
+			System.out.println("Something Went wrong");
+		}
+	}
+}
--- code/corejava/workspace/ExceptionHandling/src/ExceptionIntro.java
+++ code/corejava/workspace/ExceptionHandling/src/ExceptionIntro.java
@@ -10,8 +10,16 @@ public class ExceptionIntro {
 		
 		System.out.println("Add : " + (num1 + num2));
 		System.out.println("Sub : " + (num1 - num2));
-		System.out.println("Div : " + (num1 / num2));
+		try {
+			System.out.println("Div : " + (num1 / num2));
+			System.out.println("Mod : " + (num1 % num2));
+		} 
+		catch(ArithmeticException ex) {
+			System.out.println("Cannot Divide by Zero....");
+			/*System.out.println(ex.getClass());
+			System.out.println(ex.getMessage());
+			ex.printStackTrace();*/
+		}
 		System.out.println("Mul : " + (num1 * num2));
-		System.out.println("Mod : " + (num1 % num2));
 	}
 }
